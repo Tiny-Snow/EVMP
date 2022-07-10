cce802355eb0e3893a3cb52ea4a1ff0d0e6ec5a2
Tiny Snow <futurelover10032@gmail.com>
2022-07-10 12:18:40 +0800

    update Baseline-XGBoost

--- Baseline-XGBoost/main.py
+++ Baseline-XGBoost/main.py
@@ -4,7 +4,7 @@
 # Time:     2022.7.9
 
 import config as cfg
-from model.XGBoost import run
+from model.BaselineXGBoost import run
 
 if __name__ == '__main__':
     run(cfg.param_xgb,  cfg.param_grid)
