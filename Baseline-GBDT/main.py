4f8ef7a20af855bd3d2d9e17831bc6cd78ef8bd7
Tiny Snow <futurelover10032@gmail.com>
2022-07-10 15:45:02 +0800

    upload GBDT

---
+++ Baseline-GBDT/main.py
@@ -0,0 +1,10 @@
+# -*- coding:UTF-8 -*-
+# Author:   Raffica
+# Project:  EVMP BASELINES -- GBDT
+# Time:     2022.7.9
+
+import config as cfg
+from model.BaselineGBDT import run
+
+if __name__ == '__main__':
+    run()
