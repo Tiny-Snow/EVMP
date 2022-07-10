4960f875d9837d027d914e8b2930c536959d1645
Tiny Snow <futurelover10032@gmail.com>
2022-07-10 12:39:46 +0800

    upload RF

---
+++ Baseline-RF/main.py
@@ -0,0 +1,10 @@
+# -*- coding:UTF-8 -*-
+# Author:   Raffica
+# Project:  EVMP BASELINES -- RF
+# Time:     2022.7.9
+
+import config as cfg
+from model.BaselineRamdomForest import run
+
+if __name__ == '__main__':
+    run()
