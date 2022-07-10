2555c3ab3753e020e3b51ed4fc13254a676f9b91
Tiny Snow <futurelover10032@gmail.com>
2022-07-10 12:23:03 +0800

    upload Baseline-SVM

---
+++ Baseline-SVM/main.py
@@ -0,0 +1,10 @@
+# -*- coding:UTF-8 -*-
+# Author:   Raffica
+# Project:  EVMP BASELINES -- SVM
+# Time:     2022.7.9
+
+import config as cfg
+from model.BaselineSVM import run
+
+if __name__ == '__main__':
+    run()
